aa9ff372c7b5bc5c35b7c7788b867d9a5c13970e
Anurag Roy <anuragr9847@gmail.com>
2026-02-21 15:29:20 +0530

    Revert dwmlogo patch and update the rest

--- config.def.h
+++ config.def.h
@@ -3,8 +3,7 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[BACKGROUND] = "black",     /* after initialization */
-	[INIT] = "#2d2d2d",     /* after initialization */
+	[INIT] = "black",       /* after initialization */
 	[INPUT] = "#005577",    /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 	[CAPS] = "red",         /* CapsLock on */
@@ -16,35 +15,14 @@ static int failonclear = 1;
 /* time in seconds before the monitor shuts down */
 static int monitortime = 5;
 
-/* insert grid pattern with scale 1:1, the size can be changed with logosize */
-static int logosize = 75;
-static const int logow = 12;	/* grid width and height for right center alignment*/
-static const int logoh = 6;
-
-static XRectangle rectangles[9] = {
-	/* x	y	w	h */
-	{ 0,	3,	1,	3 },
-	{ 1,	3,	2,	1 },
-	{ 0,	5,	8,	1 },
-	{ 3,	0,	1,	5 },
-	{ 5,	3,	1,	2 },
-	{ 7,	3,	1,	2 },
-	{ 8,	3,	4,	1 },
-	{ 9,	4,	1,	2 },
-	{ 11,	4,	1,	2 },
-
-};
-
 /*
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
 		{ "color0",       STRING,  &colorname[INIT] },
-		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color3",       STRING,  &colorname[INPUT] },
 		{ "color1",       STRING,  &colorname[FAILED] },
-		{ "color3",       STRING,  &colorname[CAPS] },
-		{ "color2",       STRING,  &colorname[BACKGROUND] },
+		{ "color2",       STRING,  &colorname[CAPS] },
 		{ "failonclear", INTEGER,  &failonclear },
 		{ "monitortime", INTEGER,  &monitortime },
-		{ "logosize",    INTEGER,  &logosize },
 };
--- slock.c
+++ slock.c
@@ -1,6 +1,6 @@
 /* See LICENSE file for license details. */
-#define _XOPEN_SOURCE   500
-#define LENGTH(X)       (sizeof X / sizeof X[0])
+#include <X11/Xmd.h>
+#define _XOPEN_SOURCE 500
 #if HAVE_SHADOW_H
 #include <shadow.h>
 #endif
@@ -18,15 +18,11 @@
 #include <spawn.h>
 #include <sys/types.h>
 #include <X11/extensions/Xrandr.h>
-#ifdef XINERAMA
-#include <X11/extensions/Xinerama.h>
-#endif
 #include <X11/extensions/dpms.h>
 #include <X11/keysym.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/XKBlib.h>
-#include <X11/Xft/Xft.h>
 #include <X11/Xresource.h>
 
 #include "arg.h"
@@ -35,12 +31,24 @@
 char *argv0;
 
 enum {
-	BACKGROUND,
 	INIT,
 	INPUT,
 	FAILED,
 	CAPS,
 	NUMCOLS
+};
+
+struct lock {
+	int screen;
+	Window root, win;
+	Pixmap pmap;
+	unsigned long colors[NUMCOLS];
+};
+
+struct xrandr {
+	int active;
+	int evbase;
+	int errbase;
 };
 
 /* Xresources preferences */
@@ -57,24 +65,6 @@ typedef struct {
 } ResourcePref;
 
 #include "config.h"
-
-struct lock {
-	int screen;
-	Window root, win;
-	Pixmap pmap;
-	unsigned long colors[NUMCOLS];
-	unsigned int x, y;
-	unsigned int xoff, yoff, mw, mh;
-	Drawable drawable;
-	GC gc;
-	XRectangle rectangles[LENGTH(rectangles)];
-};
-
-struct xrandr {
-	int active;
-	int evbase;
-	int errbase;
-};
 
 static void
 die(const char *errstr, ...)
@@ -155,32 +145,6 @@ gethash(void)
 }
 
 static void
-resizerectangles(struct lock *lock)
-{
-	int i;
-
-	for (i = 0; i < LENGTH(rectangles); i++){
-		lock->rectangles[i].x = (rectangles[i].x * logosize)
-                                + lock->xoff + ((lock->mw) / 2) - (logow / 2 * logosize);
-		lock->rectangles[i].y = (rectangles[i].y * logosize)
-                                + lock->yoff + ((lock->mh) / 2) - (logoh / 2 * logosize);
-		lock->rectangles[i].width = rectangles[i].width * logosize;
-		lock->rectangles[i].height = rectangles[i].height * logosize;
-	}
-}
-
-static void
-drawlogo(Display *dpy, struct lock *lock, int color)
-{
-	XSetForeground(dpy, lock->gc, lock->colors[BACKGROUND]);
-	XFillRectangle(dpy, lock->drawable, lock->gc, 0, 0, lock->x, lock->y);
-	XSetForeground(dpy, lock->gc, lock->colors[color]);
-	XFillRectangles(dpy, lock->drawable, lock->gc, lock->rectangles, LENGTH(rectangles));
-	XCopyArea(dpy, lock->drawable, lock->win, lock->gc, 0, 0, lock->x, lock->y, 0, 0);
-	XSync(dpy, False);
-}
-
-static void
 readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens,
        const char *hash)
 {
@@ -256,7 +220,10 @@ readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens,
 			color = len ? (caps ? CAPS : INPUT) : (failure || failonclear ? FAILED : INIT);
 			if (running && oldc != color) {
 				for (screen = 0; screen < nscreens; screen++) {
-					drawlogo(dpy, locks[screen], color);
+					XSetWindowBackground(dpy,
+					                     locks[screen]->win,
+					                     locks[screen]->colors[color]);
+					XClearWindow(dpy, locks[screen]->win);
 				}
 				oldc = color;
 			}
@@ -291,10 +258,6 @@ lockscreen(Display *dpy, struct xrandr *rr, int screen)
 	XColor color, dummy;
 	XSetWindowAttributes wa;
 	Cursor invisible;
-#ifdef XINERAMA
-	XineramaScreenInfo *info;
-	int n;
-#endif
 
 	if (dpy == NULL || screen < 0 || !(lock = malloc(sizeof(struct lock))))
 		return NULL;
@@ -308,31 +271,12 @@ lockscreen(Display *dpy, struct xrandr *rr, int screen)
 		lock->colors[i] = color.pixel;
 	}
 
-	lock->x = DisplayWidth(dpy, lock->screen);
-	lock->y = DisplayHeight(dpy, lock->screen);
-#ifdef XINERAMA
-	if ((info = XineramaQueryScreens(dpy, &n))) {
-		lock->xoff = info[0].x_org;
-		lock->yoff = info[0].y_org;
-		lock->mw = info[0].width;
-		lock->mh = info[0].height;
-	} else
-#endif
-	{
-		lock->xoff = lock->yoff = 0;
-		lock->mw = lock->x;
-		lock->mh = lock->y;
-	}
-	lock->drawable = XCreatePixmap(dpy, lock->root,
-            lock->x, lock->y, DefaultDepth(dpy, screen));
-	lock->gc = XCreateGC(dpy, lock->root, 0, NULL);
-	XSetLineAttributes(dpy, lock->gc, 1, LineSolid, CapButt, JoinMiter);
-
 	/* init */
 	wa.override_redirect = 1;
-	wa.background_pixel = lock->colors[BACKGROUND];
+	wa.background_pixel = lock->colors[INIT];
 	lock->win = XCreateWindow(dpy, lock->root, 0, 0,
-	                          lock->x, lock->y,
+	                          DisplayWidth(dpy, lock->screen),
+	                          DisplayHeight(dpy, lock->screen),
 	                          0, DefaultDepth(dpy, lock->screen),
 	                          CopyFromParent,
 	                          DefaultVisual(dpy, lock->screen),
@@ -341,8 +285,6 @@ lockscreen(Display *dpy, struct xrandr *rr, int screen)
 	invisible = XCreatePixmapCursor(dpy, lock->pmap, lock->pmap,
 	                                &color, &color, 0, 0);
 	XDefineCursor(dpy, lock->win, invisible);
-
-	resizerectangles(lock);
 
 	/* Try to grab mouse pointer *and* keyboard for 600ms, else fail the lock */
 	for (i = 0, ptgrab = kbgrab = -1; i < 6; i++) {
@@ -364,7 +306,6 @@ lockscreen(Display *dpy, struct xrandr *rr, int screen)
 				XRRSelectInput(dpy, lock->win, RRScreenChangeNotifyMask);
 
 			XSelectInput(dpy, lock->root, SubstructureNotifyMask);
-			drawlogo(dpy, lock, INIT);
 			return lock;
 		}
 
@@ -455,6 +396,7 @@ main(int argc, char **argv) {
 	Display *dpy;
 	int s, nlocks, nscreens;
 	CARD16 standby, suspend, off;
+	BOOL dpms_state;
 
 	ARGBEGIN {
 	case 'v':
@@ -520,7 +462,9 @@ main(int argc, char **argv) {
 	/* DPMS magic to disable the monitor */
 	if (!DPMSCapable(dpy))
 		die("slock: DPMSCapable failed\n");
-	if (!DPMSEnable(dpy))
+	if (!DPMSInfo(dpy, &standby, &dpms_state))
+		die("slock: DPMSInfo failed\n");
+	if (!DPMSEnable(dpy) && !dpms_state)
 		die("slock: DPMSEnable failed\n");
 	if (!DPMSGetTimeouts(dpy, &standby, &suspend, &off))
 		die("slock: DPMSGetTimeouts failed\n");
@@ -545,15 +489,11 @@ main(int argc, char **argv) {
 	/* everything is now blank. Wait for the correct password */
 	readpw(dpy, &rr, locks, nscreens, hash);
 
-	for (nlocks = 0, s = 0; s < nscreens; s++) {
-		XFreePixmap(dpy, locks[s]->drawable);
-		XFreeGC(dpy, locks[s]->gc);
-	}
-
 	/* reset DPMS values to inital ones */
 	DPMSSetTimeouts(dpy, standby, suspend, off);
-
+	if (!dpms_state)
+		DPMSDisable(dpy);
 	XSync(dpy, 0);
-	XCloseDisplay(dpy);
+
 	return 0;
 }
